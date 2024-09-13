8d5ed4c7b350bb36157e1733635f96e2411f1ba9
SnehaK <khambesnehal09@gmail.com>
2024-09-13 22:49:49 +0530

    Move code from Working Dir to Remote repo

---
+++ src/test/java/jenkins/FinalJenkins_Execution.java
@@ -0,0 +1,15 @@
+package jenkins;
+
+import org.testng.annotations.Test;
+
+public class FinalJenkins_Execution 
+{
+
+	@Test
+	public void executeCode()
+	{
+		String str = "Sneha";
+		System.out.println("Below is the output of Maven project Execution: "+str);
+	}
+	
+}
